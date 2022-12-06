01e65e569959687eafa2e79b470f58e87ffdf753
aaronjan98 <aaronjan98@gmail.com>
2022-12-06 10:45:07 -0800

    add cost for mint and write test for cost

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.0;
 import './ERC721Enumerable.sol';
 
 contract NFT is ERC721Enumerable {
+    uint256 public cost;
+
     constructor(
         string memory _name,
-        string memory _symbol
-    ) ERC721(_name, _symbol) {}
+        string memory _symbol,
+        uint256 _cost
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+    }
 }
--- test/NFT.js
+++ test/NFT.js
@@ -10,21 +10,26 @@ const ether = tokens
 describe('NFT', () => {
   const NAME = 'Dapp Punks'
   const SYMBOL = 'DP'
+  const COST = ether(10)
 
   let nft
 
-  beforeEach(async () => {
-    const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy('Dapp Punks', 'DP')
-  })
-
   describe('Deployment', () => {
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory('NFT')
+      nft = await NFT.deploy(NAME, SYMBOL, COST)
+    })
+
     it('has correct name', async () => {
       expect(await nft.name()).to.equal(NAME)
     })
 
     it('has correct symbol', async () => {
       expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns the cost to mint', async () => {
+      expect(await nft.cost()).to.equal(COST)
     })
   })
 })
