29e4e3e7b793b629951cfdc4757c7131a0370061
aaronjan98 <aaronjan98@gmail.com>
2022-12-08 23:53:46 -0800

    initiate contract in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import { Container } from 'react-bootstrap'
 import { ethers } from 'ethers'
 
 // Components
-import Navigation from './Navigation';
-import Loading from './Loading';
+import Navigation from './Navigation'
+import Loading from './Loading'
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from '../abis/NFT.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [nft, setNFT] = useState(null)
+
   const [account, setAccount] = useState(null)
   const [balance, setBalance] = useState(0)
 
@@ -21,9 +24,22 @@ function App() {
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const { chainId } = provider.getNetwork()
+
+    // Initiate contract
+    const nft = new ether.Contract(
+      config[chainId].nft.address,
+      NFT_ABI,
+      provider
+    )
+    setNFT(nft)
 
     // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
 
@@ -39,24 +55,26 @@ function App() {
     if (isLoading) {
       loadBlockchainData()
     }
-  }, [isLoading]);
+  }, [isLoading])
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className="my-4 text-center">React Hardhat Template</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
+          <p className="text-center">
+            <strong>Your ETH Balance:</strong> {balance} ETH
+          </p>
+          <p className="text-center">Edit App.js to add your code here.</p>
         </>
       )}
     </Container>
   )
 }
 
-export default App;
+export default App
