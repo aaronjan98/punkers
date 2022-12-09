7b160ce752b33bd263d53e6dbe67ce407316ca04
aaronjan98 <aaronjan98@gmail.com>
2022-12-08 16:47:49 -0800

    create script to see all account balances

---
+++ scripts/getBalances.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat')
+const config = require('../src/config.json')
+const NFT_ABI = require('../src/abis/NFT.json')
+
+async function main() {
+  let accounts, deployer, minter
+
+  // Create the accounts
+  accounts = await ethers.getSigners()
+  deployer = accounts[0]
+  minter = accounts[1]
+
+  // print all users balances
+  for (let i = 0; i < accounts.length; i++) {
+    account = accounts[i].address
+    deployerBalance = await ethers.provider.getBalance(account)
+    console.log(
+      `${account} ${await ethers.utils.formatEther(deployerBalance.toString())}`
+    )
+  }
+
+  // Fetch network
+  const { chainId } = await ethers.provider.getNetwork()
+
+  // Fetch deployed token
+  const nft = await ethers.getContractAt('NFT', config[chainId].nft.address)
+  // console.log(`NFT fetched: ${nft.address}\n`)
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
